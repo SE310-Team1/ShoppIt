6901891e4ad2fdfb82470c042f79f6d92ed6ab62
Owen Eng <oeng099@aucklanduni.ac.nz>
2022-08-22 02:20:45 +1200

    Added search and searchList method to SearchPopUpController class

--- ShoppIt/src/main/application/SearchPopUpController.java
+++ ShoppIt/src/main/application/SearchPopUpController.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -20,14 +21,32 @@ public class SearchPopUpController implements Initializable {
 	@FXML
 	ListView<String> searchResultsList;
 	
-	List<String> foodItems;
+	List<String> foodItemNames;
+	
+	public void search(ActionEvent event) {
+		String query = searchTextField.getText();
+		List<String> filteredList = searchList(query,foodItemNames);
+		searchResultsList.getItems().addAll(filteredList);
+	}
+	
+	public List<String> searchList(String query, List<String> listOfStrings) {
+		List<String> filteredList = new ArrayList<>();
+		
+		for(String item: listOfStrings) {
+			if(item.toLowerCase().contains(query.toLowerCase())) {
+				filteredList.add(item);
+			}
+		}
+		
+		return filteredList;
+	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		//TO-DO: Need code to get all the food items from database and put in foodItems list.
 		//Do this once retrieval method is implemented. 
-		searchResultsList.getItems().addAll(foodItems);
+		searchResultsList.getItems().addAll(foodItemNames);
 		
 	}
 	
