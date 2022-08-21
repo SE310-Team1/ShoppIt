61e66601fd14ddbafadecfaf5d5f6d56d9f64c5a
Owen Eng <oeng099@aucklanduni.ac.nz>
2022-08-22 00:07:01 +1200

    Replaced SearchScene with SearchPopUpScene

--- ShoppIt/src/main/application/Main.java
+++ ShoppIt/src/main/application/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 
 		try {
 			Scene scene = new Scene(new Pane(), 800, 600);
-			scene.getStylesheets().add(getClass().getResource("/resources/css/application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
 			// scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Fredoka+One");
 
 			setScreens(scene);
@@ -39,7 +39,6 @@ public class Main extends Application {
 		ScreenHandler.add("main", "Main.fxml");
 		ScreenHandler.add("individualListScene", "IndividualListScene.fxml");
 		ScreenHandler.add("newListScene", "NewListScene.fxml");
-		ScreenHandler.add("searchScene", "SearchScene.fxml");
 		ScreenHandler.changeTo("main"); // starting screen
 	}
 
