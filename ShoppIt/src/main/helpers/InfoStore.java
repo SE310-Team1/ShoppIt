bfb1f8d6dd7d3abbe6f887e6eaa9b1fafb630878
poli648 <79772678+poli648@users.noreply.github.com>
2022-09-30 11:06:06 +1300

    Added list to main scene (#96)
    * feat: adding to list now functions (#89)
    
    * Revert "feat: adding to list now functions (#89)" (#91)
    
    This reverts commit c9ad398280af7b8948675163bc78c601475aae95.
    
    * added the methods to add listId to items
    
    * checkpoint to test new changes
    
    * implememented fix for lists
    
    * added the clearing of cached results
    
    * finally fixed the listId counting issue
    
    * added comments to data classes
    
    Co-authored-by: jzho987 <79814775+jzho987@users.noreply.github.com>
    Co-authored-by: ochu761 <79777245+ochu761@users.noreply.github.com>

--- ShoppIt/src/main/helpers/InfoStore.java
+++ ShoppIt/src/main/helpers/InfoStore.java
@@ -1,12 +1,15 @@
 package helpers;
 
 import java.util.List;
+
+import database.DatabaseManager;
 import database.models.Item;
 
 public class InfoStore {
 
 	private static InfoStore instance = null;
     private List<Item> itemList;
+    private static int listId = -1;
     
     public static InfoStore getInstance() {
 		if (instance == null) {
@@ -19,8 +22,36 @@ public class InfoStore {
         this.itemList = itemList;
     }
 
+    public void setList(List<Item> itemList, int listID){
+        this.itemList = itemList;
+        this.listId = listID;
+    }
     public List<Item> getList() {
         return itemList;
     }
 
+    public void persistList(){
+
+        DatabaseManager databaseManager = new DatabaseManager();
+
+        //checks if id has already been provided to the infoStore
+        if(listId == -1){
+            listId = (int)databaseManager.newestListId();
+            listId++;
+        }
+
+        for (Item item: itemList) {
+            //If an item is not part of a list, then set it list Id to be that of the current list
+            if(item.getListId() == 0) {
+                item.setListId(listId);
+                databaseManager.addObject(item);
+            }
+        }
+       resetInfoStore();
+    }
+
+    public void resetInfoStore(){
+        itemList.clear();
+        listId = -1;
+    }
 }
