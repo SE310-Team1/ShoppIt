9519c8fad5536c6b9dfd3cb6372b448a8cad3fd0
samanthamebius <79612950+samanthamebius@users.noreply.github.com>
2022-10-18 16:14:28 +1300

    Removed backgrounds from item images (#121)
    * added images with removed backgrounds
    
    * converted all jpg image references to png
    
    * corrected poorly updated images
    
    * removed unneeded jpg images
    
    * cropped images to fill containers
    
    * update file names in db
    
    * removed code to update db
    
    * Revert "removed code to update db"
    
    This reverts commit a712c7e2dcaac44ff384035695252d9a8a0dba92.
    
    * Revert "update file names in db"
    
    This reverts commit 0370e97a9499048a7e82dde477bd4c24f39336fb.
    
    * reupdate images from jpg to png
    
    Co-authored-by: Ou-An <79777245+ochu761@users.noreply.github.com>
    Co-authored-by: @ktha423

--- ShoppIt/src/main/controllers/MainController.java
+++ ShoppIt/src/main/controllers/MainController.java
@@ -42,6 +42,48 @@ public class MainController implements Initializable {
         colourImages();
 
         DatabaseManager DB = new DatabaseManager();
+        // TODO: remove all this when all pull requests are complete
+        DB.updateImage("Apples", "apple.png");
+        DB.updateImage("Bananas", "banana.png");
+        DB.updateImage("Broccoli", "broccoli.png");
+        DB.updateImage("Carrots", "carrots.png");
+        DB.updateImage("Chicken breast", "chicken_breast.png");
+        DB.updateImage("Eggs", "egg.png");
+        DB.updateImage("Greek yogurt", "greek_yogurt.png");
+        DB.updateImage("Milk", "milk.png");
+        DB.updateImage("Olive oil", "olive_oil.png");
+        DB.updateImage("Pasta", "pasta.png");
+        DB.updateImage("Pistachios", "pistachios.png");
+        DB.updateImage("Potatoes", "potatoes.png");
+        DB.updateImage("Rice", "rice.png");
+        DB.updateImage("Spinach", "spinach.png");
+        DB.updateImage("Steak", "steak.png");
+        DB.updateImage("Tasty cheese", "tasty_cheese.png");
+        DB.updateImage("White bread", "tip_top_bread.png");
+        DB.updateImage("Ice cream", "tip_top_ice_cream.png");
+        DB.updateImage("Tomatoes", "tomato.png");
+        DB.updateImage("Hazelnut Chocolate", "whittakers_hazelnut.png");
+        DB.updateImage("Avocado", "avocado.png");
+        DB.updateImage("Avocado", "avocado.png");
+        DB.updateImage("Capsicum", "capsicum.png");
+        DB.updateImage("Butter", "butter.png");
+        DB.updateImage("Cream", "cream.png");
+        DB.updateImage("Spring Onion", "spring_onions.png");
+        DB.updateImage("Camembert", "camembert.png");
+        DB.updateImage("Lamb", "lamb.png");
+        DB.updateImage("Mushroom", "mushrooms.png");
+        DB.updateImage("Wholemeal bread", "tip_top_wholemeal_bread.png");
+        DB.updateImage("Tuna", "tuna.png");
+        DB.updateImage("Edam cheese", "edam_cheese.png");
+        DB.updateImage("Chocolate chippies Cookies", "chocolate_chippies_cookies.png");
+        DB.updateImage("Stripes cookies", "stripes_cookies.png");
+        DB.updateImage("Berry yogurt", "berry_yogurt.png");
+        DB.updateImage("Shaved ham", "beehive_ham.png");
+        DB.updateImage("Sesameal crackers", "sesameal_crackers.png");
+        DB.updateImage("Weet-Bix", "weet_bix.png");
+        DB.updateImage("Canola oil", "canola.png");
+        DB.updateImage("Dairy Milk Bar", "dairy_milk_bar.png");
+        DB.updateImage("Sour Patch Kids", "sour_patch_kids.png");
         try {
             lists = DB.getTable(ShoppingList.class, "ShoppingList");
 
